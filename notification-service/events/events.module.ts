961b20c62bfd385165b27409a8ddf31a5166cbf2
Sadoune <Sadounefadjre321@gmail.com>
2026-05-02 19:06:30 +0100

    Refactor EventsModule to include StorageListener and controller

--- notification-service/events/events.module.ts
+++ notification-service/events/events.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { ScheduleModule } from '@nestjs/schedule';
+
 import { ReservationListener } from './reservation.listener';
+import { StorageListener } from './storage.listener';
+import { StorageController } from './storage.controller';
 import { NotificationModule } from '../notification/notification.module';
 
 @Module({
-  imports: [HttpModule, ScheduleModule.forRoot(), NotificationModule],
-  providers: [ReservationListener],
+  imports: [
+    HttpModule,
+    ScheduleModule.forRoot(),
+    NotificationModule,
+  ],
+  controllers: [StorageController],
+  providers: [ReservationListener, StorageListener],
 })
 export class EventsModule {}
